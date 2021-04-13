6c9a6f39fb3def58d0131068aff5e832920d7ceb
KayKozaronek <kozaronek@gmail.com>
2021-04-13 14:59:50 +0200

    implementation of LinearDeepQNetwork

---
+++ naive_deep_q/cartpole_naive_dqn.py
@@ -0,0 +1,25 @@
+import gym 
+import numpy as np 
+import torch.nn as nn
+import torch.nn.functional as F 
+import torch optim as optim 
+import torch 
+
+class LinearDeepQNetwork(nn.Module):
+    def __init__(self, lr, n_actions, input_dims):
+        super().__init__()
+
+        self.fc1 = nn.Linear(*input_dims, 128)
+        self.fc2 = nn.Linear(128, n_actions)
+        
+        self.optimizer = optim.Adam(self.parameters, lr=lr)
+        self.loss = nn.MSELoss()
+        
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.to(self.device)
+
+    def forward(self, state):
+        layer1 = F.relu(self.fc1(state))
+        actions = self.fc2(layer1)
+
+        return actions 
